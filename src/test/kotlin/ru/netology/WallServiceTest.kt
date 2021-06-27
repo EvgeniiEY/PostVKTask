30067512328e8fee12bed73110c6eda465e30eb7
Evgenii Yakhontov <yakhontov.e@yandex.ru>
2021-06-27 21:57:46 +0300

    third task

--- src/test/kotlin/ru/netology/WallServiceTest.kt
+++ src/test/kotlin/ru/netology/WallServiceTest.kt
@@ -1,10 +1,10 @@
 package ru.netology
 
 import Post
-import WallService.posts
+import WallService
 import org.junit.Test
-
 import org.junit.Assert.*
+
 
 class WallServiceTest {
 
@@ -14,7 +14,98 @@ class WallServiceTest {
     }
 
     @Test
-    fun update() {
-        assertEquals(false, true)
+    fun updateExisting() {
+        // создаём целевой сервис
+        val service = WallService
+        // заполняем несколькими постами
+        service.add(
+            Post(
+                1,
+                1,
+                1,
+                1,
+                "",
+                1,
+                1,
+                1,
+                1,
+                true,
+                true,
+                true,
+                true,
+                1,
+                "",
+                "",
+                "",
+                1,
+                "",
+                true,
+                true,
+                1,
+                true,
+                1,
+                "",
+                1,
+                true,
+                true,
+                true,
+                1,
+                true,
+                true,
+                1,
+                true,
+                true,
+                "",
+                1
+            )
+        )
+
+//        service.add(Post(/* заполняете поля */))
+////        service.add(Post(/* заполняете поля */))
+//        // создаём информацию об обновлении
+        val update = Post(
+            1,
+            1,
+            1,
+            1,
+            "",
+            1,
+            1,
+            1,
+            1,
+            true,
+            true,
+            true,
+            true,
+            1,
+            "",
+            "",
+            "",
+            1,
+            "",
+            true,
+            true,
+            1,
+            true,
+            1,
+            "",
+            1,
+            true,
+            true,
+            true,
+            1,
+            true,
+            true,
+            1,
+            true,
+            true,
+            "",
+            1
+        )
+        // выполняем целевое действие
+        val result = service.update(update)
+
+        // проверяем результат (используйте assertTrue или assertFalse)
+        assertTrue(result)
     }
 }
