6526ba8d158be52c5c024fc4a95da852be28d2a4
Evgenii Yakhontov <yakhontov.e@yandex.ru>
2021-06-27 19:07:58 +0300

    second task with crash

---
+++ src/test/kotlin/ru/netology/WallServiceTest.kt
@@ -0,0 +1,18 @@
+package ru.netology
+
+import Post
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class WallServiceTest {
+
+    @Test
+    fun add() {
+    }
+
+    @Test
+    fun update() {
+        assertEquals(false, true)
+    }
+}
